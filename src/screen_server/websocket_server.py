1abdf249798029b77731825985c3d44e156a9d68
Eulogio Quemada <elgameshdpay@gmail.com>
2024-06-16 18:58:53 +0200

    Refactoring

--- src/screen_server/websocket_server.py
+++ src/screen_server/websocket_server.py
@@ -1,35 +1,33 @@
-import asyncio
-import websockets
 import json
 import cv2
-import numpy as np
-import threading
-from mss import mss
-import pyautogui
 import time
 import signal
+
+import threading
+import asyncio
+import websockets
+
 import dxcam
+import pyautogui
+
 
 class WRC:
 
-    def __init__(self):
+    def __init__(self, get_last_frame, process_message):
+        self.get_last_frame = get_last_frame
+        self.process_message = process_message
+
         self.clients = []
-        self.sct = mss()
 
         self.screen_width = pyautogui.size().width
         self.screen_height = pyautogui.size().height
 
-        self.camera = dxcam.create()
-        self.camera.start(target_fps=60)
+        self.camera = dxcam.create(output_color="BGR")
+        self.camera.start(target_fps=120)
 
         self.active = True
 
-    def click_mouse(self, x, y):
-        (old_x, old_y) = pyautogui.position()
-        pyautogui.click(x, y)
-        pyautogui.moveTo(old_x, old_y)
-
-    def spin(self):
+    def screen_share(self):
         while self.active:
             if self.clients:
                 try:
@@ -41,30 +39,19 @@ class WRC:
                     jpeg_bytes= cv2.imencode(".jpg", screen_np)[1].tobytes()
                     jpg_time = time.time() - (capture_time + start_time)
                 
-                    asyncio.run(self.send_frames(jpeg_bytes))
+                    asyncio.run(self.send_frame(jpeg_bytes))
                     send_time = time.time() - (jpg_time + capture_time + start_time)
 
-                    elapsed_time = time.time() - start_time
-                    print(f"FPS de envío: {round(1 / elapsed_time, 2)}, CAPTURE: {round(capture_time,4)}" +
-                        f", JPG: {round(jpg_time,4)}, SEND: {round(send_time,4)}")
+                    print(f"FPS de envío: {round(1 / (time.time() - start_time), 2)}, " +
+                          f"CAPTURE: {round(capture_time, 4)}" +
+                          f", JPG: {round(jpg_time, 4)}, SEND: {round(send_time, 4)}")
                 except Exception as e:
                     print(">> ERROR: " + str(e))
             else:
-                time.sleep(0.1)  # Evitar consumo excesivo de CPU cuando no hay clientes
+                time.sleep(0.1)
 
         print("Saliendo del bucle de envio...")
-
-    async def send_frames(self, jpeg_bytes):
-        tasks = [asyncio.create_task(self.send_to_client(client, jpeg_bytes)) for client in self.clients]
-
-        await asyncio.wait(tasks)
-
-    async def send_to_client(self, client, message):
-        try:
-            await client.send(message)
-        except Exception:
-            pass
-
+            
     async def echo(self, websocket, _):
         self.clients.append(websocket)
         print(f">> ENTRADA: Nuevo cliente conectado: {websocket.remote_address[0]}:{websocket.remote_address[1]} (Conexiones activas: {len(self.clients)})")
@@ -81,12 +68,24 @@ class WRC:
                 real_y = self.screen_height * (y / img_h)
 
                 print("Coordenadas del ratón:", int(real_x), int(real_y))
-                self.click_mouse(real_x, real_y)
+                self.do_click(real_x, real_y)
         except Exception as e:
             print(">> ERROR AL RECIBIR: " + str(e))
         finally:
             self.clients.remove(websocket)
             print(f">> SALIDA: Cliente desconectado: {websocket.remote_address[0]}:{websocket.remote_address[1]} (Conexiones activas: {len(self.clients)})")
+
+    def do_click(self, x, y):
+        (old_x, old_y) = pyautogui.position()
+        pyautogui.click(x, y)
+        pyautogui.moveTo(old_x, old_y)
+
+    async def send_frame(self, jpeg_bytes):     
+        try:
+            for client in self.clients:
+                await client.send(jpeg_bytes)
+        except Exception:
+            pass
 
     async def socket_thread(self):
         print("Iniciando websocket...")
@@ -94,24 +93,23 @@ class WRC:
         async with websockets.serve(self.echo, "0.0.0.0", 8765):
             while self.active:
                 await asyncio.sleep(1)
-        print("Deteniendo websocket...")
+        print("Cerrando websocket...")
 
     def execute_socket_thread(self):
         asyncio.run(self.socket_thread())
 
-wrc = WRC()
-def signal_handler(sig, frame):
-    wrc.active = False
-    print("\nCtrl+C detectado. Apagando...")
+    def signal_handler(self, sig, frame):
+        self.active = False
+        print("\CTRL + C detectado. Apagando...")
 
 def main(args=None):
-    signal.signal(signal.SIGINT, signal_handler)
+    wrc = WRC()
+    signal.signal(signal.SIGINT, wrc.signal_handler)
 
     socket_thread = threading.Thread(target=wrc.execute_socket_thread)  # Escucha clientes
     socket_thread.start()
 
-    wrc.spin()  # Manda frames
-    socket_thread.join()
+    wrc.screen_share()
 
 if __name__ == "__main__":
     main()
